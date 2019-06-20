a0cd3b14678f9ad3b80d29d4f5b13906005c6415
AnuyaPadte <anuya.padte@somaiya.edu>
2019-06-20 19:09:32 +0530

    Navigation Changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
   def home
-
+    if user_signed_in?
+      redirect_to transactions_path
+    else
+      redirect_to new_user_session_path
+    end
   end
 end
